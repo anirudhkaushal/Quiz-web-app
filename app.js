b22f677a54603e335ad31327a4421ba2bae4a6cd
anirudhkaushal <69610899+anirudhkaushal@users.noreply.github.com>
2021-01-02 16:40:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,16 +10,14 @@ form.addEventListener("submit", e => {
 
     const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value, form.q6.value, form.q7.value, form.q8.value, form.q9.value, form.q10.value];
 
-    // check answers
     userAnswers.forEach((answer, index) => {
         if(answer === correctAnswers[index]) {
             score += 10;
         }
     });
 
-    //show result on page
-    // the scrollTo() function belogs to the window object
-    scrollTo(0,0);    // arguments are x-coordinate and y-coordinate
+  
+    scrollTo(0,0);   
     result.classList.remove("d-none");
 
     allInputs.forEach((input) => {
@@ -38,26 +36,3 @@ form.addEventListener("submit", e => {
     }, 100);
 });
 
-// window object (global object)
-
-// console.log("hello");
-// window.console.log("hello");
-
-// document.querySelector();
-// window.document.querySelector();
-
-// alert("hello");
-// window.alert("hello2");
-
-// setTimeout( () => {    // this function also belongs to the window object
-//     alert("hello");
-// }, 3000);
-
-// let i = 0;
-// const timer = setInterval( () => {
-//     console.log("hello");
-//     i++;
-//     if(i === 7) {
-//         clearInterval(timer);
-//     }
-// }, 1000);       // this function also belongs to the window object
